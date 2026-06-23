9ee98bff56bf606465af6edbcd7ab17103a491de
Sathwik <sadamsathvik@gmail.com>
2026-06-23 06:01:57 +0000

    [LeetCode Sync] Runtime - 11 ms (96.64%), Memory - 25.8 MB (70.98%)

---
+++ 0217-contains-duplicate/solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return False if(len(nums)==len(set(nums))) else True
+    
