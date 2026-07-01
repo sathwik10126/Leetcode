8023d14ff9d19bfec0c19d5431c2901083c8bc0e
Sathwik <sadamsathvik@gmail.com>
2026-07-01 05:16:11 +0000

    [LeetCode Sync] Runtime - 2104 ms (37.11%), Memory - 13.3 MB (16.82%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -5,12 +5,11 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        r={}
-        for i in range(len(nums)):
-            b=target-nums[i]
-            if b in r:
-                return [r[b],i]
-            r[nums[i]]=i
+        n=len(nums)
+        for i in range(n):
+            for j in range(i+1,n):
+                if nums[i]+nums[j]==target:
+                    return [i,j]
             
            
 
